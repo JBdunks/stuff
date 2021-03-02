4da5a5af5de72e399d0543392028b3dfb53c8eb7
Jon Butler <Jwb2472@gmail.com>
2021-03-02 02:25:32 -0500

    finished rps

--- cal/script.js
+++ cal/script.js
@@ -1,1 +1,77 @@
 console.log('simple')
+
+let operator;
+
+
+const Operators ={
+    ADD: 'add',
+    SUBTRACT: 'subtract',
+    MULTIPLY: 'multiply',
+    DIVISION: 'division'
+};
+
+const main = () =>{
+    console.log('main')
+    first = parseInt(document.getElementById('first').value);
+    second = parseInt(document.getElementById('second').value);
+    console.log(`first: ${first}  second: ${second}`)
+
+operator = getOperator();
+
+
+switch (operator) {
+    case Operators.ADD:
+        add(first, second);
+        break;
+    case Operators.SUBTRACT:
+        subtract(first, second);
+        break;
+    case Operators.MULTIPLY:
+        multiply(first,second);
+        break;
+    case Operators.DIVIDE:
+        divide(first,second);
+        break;
+    default:
+        break;                
+}
+
+}
+
+
+const getOperator = () => {
+    console.log('triggered')
+    var radioButtons = document.getElementsByName('operand')
+    radioButtons.forEach(x => {
+    if(x.checked){
+        console.log(` getOperator value ${x.value}`)
+        return x.value
+    }
+});
+return add;
+}
+
+
+const add = (fist, second) => {
+    console.log(operator)
+    console.log(first + second)
+}
+
+const subtract = (first, second) => {
+    console.log(operator)
+}
+
+const multiply = (first, second) => {
+    console.log(operator)
+}
+
+const divide = (first, second) => {
+    console.log(operator)
+if(second == 0){prompt('Cannot Divide by 0.'), reload()}
+}
+
+const reload = () => location.reload()
+
+const select = () => {
+    console.log('clicked')
+}
--- rps/rocks.js
+++ rps/rocks.js
@@ -1,52 +1,75 @@
-console.log('loaded')
-const choices = [null, 'rock', 'paper', 'scissors'];
-var user;
-var computer;
-var game;
-var counter = 0;
-var user_total = 0;
-var computer_total = 0;
 
-const rock =() =>{
+const choices = [null, 'rock', 'paper', 'scissors'];
+let user;
+let computer;
+let game;
+let count = 0;
+let user_total = 0;
+let computer_total = 0;
+
+const rock = () =>{
     user = 'rock'
     computer = Math.ceil(Math.random()*3)
-    console.log(`user: ${user}, computer: ${choices[computer]}`)
-    document.getElementById('result').innerHTML = `user: ${user}, computer: ${choices[computer]}`
-    if(computer == 1){console.log(`tie`)};
-    if(computer == 2){console.log('computer wins')};
-    if(computer == 3){console.log('you win!')}
-    counter ++
+    document.getElementById('result').innerHTML = `user : ${user}, computer : ${choices[computer]}`
+    if(computer == 2){computer_total++; counter();};
+    if(computer == 3){user_total++; counter();};
+    updateScore();
 };
 
 const paper = () =>{
     user = 'paper'
     computer = Math.ceil(Math.random()*3)
-    console.log(`user: ${user}, computer: ${choices[computer]}`)
-    document.getElementById('result').innerHTML = `user: ${user}, computer: ${choices[computer]}`
-    if(computer == 1){console.log(`you win!`)};
-    if(computer == 2){console.log('tie')};
-    if(computer == 3){console.log('computer wins')}
-    counter ++
+    document.getElementById('result').innerHTML = `user : ${user}, computer : ${choices[computer]}`
+    if(computer == 1){user_total++; counter();};
+    if(computer == 3){computer_total++; counter();};
+    updateScore();
 };
 
 const scissors = () =>{
     user = 'scissors'
     computer = Math.ceil(Math.random()*3)
-    console.log(`user: ${user}, computer: ${choices[computer]}`)
-    document.getElementById('result').innerHTML = `user: ${user}, computer: ${choices[computer]}`
-    if(computer == 1){console.log(`you lose`)};
-    if(computer == 2){console.log('you win!')};
-    if(computer == 3){console.log('tie')}
-    counter ++
+    document.getElementById('result').innerHTML = `user : ${user}, computer : ${choices[computer]}`
+    if(computer == 1){computer_total++; counter();};
+    if(computer == 2){user_total++; counter();};
+    updateScore();
 };
 
-/*
-const start = ()=>{
-     game = parseInt(prompt('How many games would you like to play?'),10)
-     console.log(typeof game)
-     //location.reload();
-}
 
-start()
-*/
-console.log(eval(1234+4321))
+const updateScore = (a,b) =>{
+    document.getElementById('user_score').innerHTML =  user_total > computer_total ?
+        `You are currently winning ${user_total} to ${computer_total}` :
+        computer_total > user_total ?
+         `The computer is currently winning ${computer_total} to ${user_total}` :
+         `The game is currently tied ${user_total} to ${computer_total}`;
+};
+
+
+const start = ()=>{
+    document.getElementById('start_btn').style.display = 'none';
+    document.getElementById('rock').disabled = false;
+    document.getElementById('paper').disabled = false;
+    document.getElementById('scissors').disabled = false;
+    game = prompt('How many games would you like to play?');
+     if((/\D/g).test(game)){
+         alert('Not a valid number.');
+         location.reload();
+     };
+};
+
+
+
+const counter = () =>{
+    count++
+    if(count == game && user_total > computer_total){
+        alert(`Congragulations!! You managed to defeat the computer ${user_total} to ${computer_total}.`);
+        location.reload();
+    };
+    if(count == game && user_total < computer_total){
+        alert(`You were deafted by the computer ${computer_total} to ${user_total}.  Better luck next time.`);
+        location.reload();
+    };
+    if(count == game && user_total == computer_total){
+        alert(`The game ended in a tie.`);
+        location.reload();
+    };
+};
